a1ad95fcba41373f061bd20fac06bbe92e5e429a
SiamIslamSagor <msiamislam12345@gmail.com>
2025-04-20 08:24:16 -0600

    remove bordwere

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-gray-800  border-4 border-blue-800`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-gray-800`}
         style={{
           backgroundImage:
             "linear-gradient(0deg, hsl(197deg 61% 55%) 0%, hsl(198deg 59% 60%) 10%, hsl(199deg 59% 65%) 20%, hsl(200deg 58% 69%) 29%, hsl(200deg 58% 72%) 38%, hsl(201deg 57% 76%) 46%, hsl(201deg 57% 80%) 54%, hsl(201deg 57% 83%) 61%, hsl(201deg 57% 86%) 69%, hsl(202deg 56% 90%) 77%, hsl(202deg 56% 93%) 84%, hsl(202deg 56% 97%) 92%, hsl(0deg 0% 100%) 100%)",
